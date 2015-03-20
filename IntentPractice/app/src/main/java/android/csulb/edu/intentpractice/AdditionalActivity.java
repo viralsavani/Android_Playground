0053093eea0214ee35eb663f1411078d3b264fe0
Viral Savani <viralsavani@gmail.com>
2015-03-20 00:07:33 -0700

    Some comments for understanding.

--- IntentPractice/app/src/main/java/android/csulb/edu/intentpractice/AdditionalActivity.java
+++ IntentPractice/app/src/main/java/android/csulb/edu/intentpractice/AdditionalActivity.java
@@ -2,10 +2,7 @@ package android.csulb.edu.intentpractice;
 
 import android.app.Activity;
 import android.content.Intent;
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -15,7 +12,6 @@ public class AdditionalActivity extends Activity {
 
     public static String RETURN_MSG = "return_msg";
 
-    private Button okButton;
     private EditText editText;
 
     @Override
@@ -23,7 +19,7 @@ public class AdditionalActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_additional);
 
-        okButton = (Button) findViewById(R.id.additionalOkButton);
+        Button okButton = (Button) findViewById(R.id.additionalOkButton);
         editText = (EditText) findViewById(R.id.additionalEditText);
 
         okButton.setOnClickListener(new View.OnClickListener() {
@@ -32,7 +28,13 @@ public class AdditionalActivity extends Activity {
                 String message = editText.getText().toString();
 
                 Intent intent = new Intent();
-                intent.putExtra(RETURN_MSG , message);
+                intent.putExtra(RETURN_MSG, message);
+
+                /**
+                 * When the called activity is ready to finish, it could return an optional resultCode
+                 * to the caller to summarize the success of its execution. The brief resultCode as
+                 * well as any additional extra data can be collected back on the parent's using
+                 */
                 setResult(Activity.RESULT_OK, intent);
                 finish();
             }
--- IntentPractice/app/src/main/java/android/csulb/edu/intentpractice/StaticActivity.java
+++ IntentPractice/app/src/main/java/android/csulb/edu/intentpractice/StaticActivity.java
@@ -1,10 +1,7 @@
 package android.csulb.edu.intentpractice;
 
 import android.app.Activity;
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
